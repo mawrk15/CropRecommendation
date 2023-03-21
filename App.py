17ced9a815f5449ffaf75c3531247236257f7c36
mawrk15 <127464628+mawrk15@users.noreply.github.com>
2023-03-21 19:16:55 +0800

    Update App.py

--- App.py
+++ App.py
@@ -55,7 +55,6 @@ st.header('Environment Data')
 st.write(user_data)
 
 crop_predict = model.predict(user_data)
-st.subheader(crop_name)
 for predict1 in range(22):
   if crop_predict[0,predict1] == 1.0:
     if predict1 == 0:                                                                                              # Above we have converted the crop names into numerical form, so that we can apply the machine learning model easily. Now we have to again change the numerical values into names of crop so that we can print it when required.
